02ef15cbc370e0234c5b50be9b04bf5734fa96ca
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-04-30 17:36:06 -0400

    update

--- backend-django/demo/urls.py
+++ backend-django/demo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from .views import Another
 urlpatterns = [
-    path('', views.first),
+    path('first', views.first),
+    path('another_view', Another.as_view())
 ]
--- backend-django/demo/views.py
+++ backend-django/demo/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from .models import Book
 
+class Another(View):
+
+    # gets all of the objects from book and places them in books
+    books = Book.objects.all();
+
+    if (len(books) == 1):
+        output = f"We have { len(books) } book in Database."
+
+    else:
+        output = f"We have { len(books) } that many books in Database."
+
+
+    def get(self, request):
+        return HttpResponse(self.output)
 # Create your views here.
+
+
 def first(request):
-    return HttpResponse('First Message from Views');
+    return HttpResponse('First Message from Views')
